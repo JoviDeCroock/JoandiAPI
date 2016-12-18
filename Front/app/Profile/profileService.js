b4d60e5f1fe71bdc4c13d4f9500de158c5639341
Jovi De Croock <jovi.decroock.u8420@student.hogent.be>
2016-12-18 13:48:51 +0100

    no message

--- Front/app/Profile/profileService.js
+++ Front/app/Profile/profileService.js
@@ -52,7 +52,7 @@
         function check(id)
         {
             console.log(id);
-            return $http.get(url.dev + id + "/isAdmin", {
+            return $http.get("http://188.166.173.147:3000/" + id + "/isAdmin", {
                 headers: {Authorization: 'Bearer ' + token}
             }).success(function(data)
             {
